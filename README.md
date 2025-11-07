ffe5b0b8d1c76f3de540820cfb2ad9ebe0a0e2fe
Saman833 <git@stackblitz.com>
2025-11-07 23:06:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hs3-ts2-jxstzmrd
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Saman833/hs3-ts2-jxstzmrd)
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+// Work through these Exercises from Bitovi Academy.
+// Disregard the "Verify" sections, as we do not have a testing environment set up yet.
+
+// https://www.bitovi.com/academy/learn-typescript/keyof-typeof.html
+// https://www.bitovi.com/academy/learn-typescript/generics.html
+// https://www.bitovi.com/academy/learn-typescript/utility-types.html
+// https://www.bitovi.com/academy/advanced-typescript/generics-with-constraints.html
+// https://www.bitovi.com/academy/advanced-typescript/mapped-types.html
+// https://www.bitovi.com/academy/advanced-typescript/conditional-types.html
